b87ae00ef8f794d8d6029bf8dc7151e1bb0ea9e9
Vitaly Bogomolov <mail@vitaly-bogomolov.ru>
2016-02-19 13:59:27 +0300

    implement width calculating

--- gae/anomaly.py
+++ gae/anomaly.py
@@ -1,3 +1,5 @@
+DFLT_FADING_PERCENT = 10
+
 class Item(object):
     """
     Extracting an array of measurements of sensor with line_index from whole array data.
@@ -23,7 +25,19 @@ class Item(object):
     >>> defect.amplitude
     4
     >>> defect.rectangle()
-    (2, 0, 3, 4)
+    (2, 0, 3, 1)
+
+    >>> defect = Item(2, dat)
+    >>> defect.sensor_data
+    [23, 32, 23, 44, 57, 43, 22, 21]
+    >>> defect.deltas
+    [0, 9, 0, 21, 34, 20, -1, -2]
+    >>> defect.h0
+    23
+    >>> defect.amplitude
+    34
+    >>> defect.rectangle()
+    (2, 0, 3, 1)
 
     """
 
@@ -44,7 +58,10 @@ class Item(object):
         """Middle level of sensor signal"""
         return self.sensor_data[0]
 
-    def rectangle(self, width_signal_fading=10):
+    def amplitude_on_interval(self, pos_left, pos_right):
+        return max(self.deltas[pos_left:pos_right])
+
+    def rectangle(self, width_signal_fading=DFLT_FADING_PERCENT):
         ampl_position = self.deltas.index(self.ampl_value)
 
         pos_left = ampl_position - 1 - index_le(reversed(self.deltas[:ampl_position]), 0)
@@ -56,7 +73,27 @@ class Item(object):
         except:
             pass
 
-        return pos_left, 0, pos_right - pos_left, len(self.source_data[0])
+        # find top border of defect in source array
+        border = 0
+        line = self.line_index
+        while line > border:
+            amplitude = Item(line, self.source_data).amplitude_on_interval(pos_left, pos_right)
+            if (amplitude > self.amplitude) or (amplitude <= (self.amplitude * width_signal_fading / 100)):
+                break
+            line -= 1
+        pos_top = line
+
+        # find bottom border of defect in source array
+        border = len(self.source_data[0]) - 1
+        line = self.line_index
+        while line < border:
+            amplitude = Item(line, self.source_data).amplitude_on_interval(pos_left, pos_right)
+            if (amplitude > self.amplitude) or (amplitude <= (self.amplitude * width_signal_fading / 100)):
+                break
+            line += 1
+        pos_bottom = line
+
+        return (pos_left, pos_top, pos_right - pos_left, pos_bottom - pos_top)
 
 def index_le(sequence, value):
     """
